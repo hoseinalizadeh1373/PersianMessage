97875b81274350009ef7c99008d8206bb2bc81d8
hosein <85564616+hoseinalizadehkhiabani@users.noreply.github.com>
2022-05-14 10:00:16 +0430

    First

---
+++ persian_message/message.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace persian_message
+{
+    partial class message
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(message));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnretry = new System.Windows.Forms.Button();
+            this.btnabort = new System.Windows.Forms.Button();
+            this.btncancel = new System.Windows.Forms.Button();
+            this.btnyes = new System.Windows.Forms.Button();
+            this.btnignore = new System.Windows.Forms.Button();
+            this.btnno = new System.Windows.Forms.Button();
+            this.btnok = new System.Windows.Forms.Button();
+            this.GroupBox1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.GroupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(86, 3);
+            this.label1.MaximumSize = new System.Drawing.Size(412, 700);
+            this.label1.MinimumSize = new System.Drawing.Size(250, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 60);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(79, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnretry
+            // 
+            this.btnretry.Image = ((System.Drawing.Image)(resources.GetObject("btnretry.Image")));
+            this.btnretry.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnretry.Location = new System.Drawing.Point(239, 7);
+            this.btnretry.Name = "btnretry";
+            this.btnretry.Size = new System.Drawing.Size(109, 44);
+            this.btnretry.TabIndex = 1;
+            this.btnretry.Text = "سعی مجدد";
+            this.btnretry.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnretry.UseVisualStyleBackColor = true;
+            this.btnretry.Click += new System.EventHandler(this.btnretry_Click);
+            // 
+            // btnabort
+            // 
+            this.btnabort.Image = ((System.Drawing.Image)(resources.GetObject("btnabort.Image")));
+            this.btnabort.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnabort.Location = new System.Drawing.Point(9, 7);
+            this.btnabort.Name = "btnabort";
+            this.btnabort.Size = new System.Drawing.Size(109, 44);
+            this.btnabort.TabIndex = 2;
+            this.btnabort.Text = "چشم پوشی";
+            this.btnabort.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnabort.UseVisualStyleBackColor = true;
+            this.btnabort.Click += new System.EventHandler(this.btnabort_Click);
+            // 
+            // btncancel
+            // 
+            this.btncancel.Image = ((System.Drawing.Image)(resources.GetObject("btncancel.Image")));
+            this.btncancel.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btncancel.Location = new System.Drawing.Point(124, 7);
+            this.btncancel.Name = "btncancel";
+            this.btncancel.Size = new System.Drawing.Size(109, 44);
+            this.btncancel.TabIndex = 3;
+            this.btncancel.Text = "انصراف";
+            this.btncancel.UseVisualStyleBackColor = true;
+            this.btncancel.Click += new System.EventHandler(this.btncancel_Click);
+            // 
+            // btnyes
+            // 
+            this.btnyes.Image = ((System.Drawing.Image)(resources.GetObject("btnyes.Image")));
+            this.btnyes.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnyes.Location = new System.Drawing.Point(239, 7);
+            this.btnyes.Name = "btnyes";
+            this.btnyes.Size = new System.Drawing.Size(109, 44);
+            this.btnyes.TabIndex = 6;
+            this.btnyes.Text = "بلی";
+            this.btnyes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnyes.UseVisualStyleBackColor = true;
+            this.btnyes.Click += new System.EventHandler(this.btnyes_Click);
+            // 
+            // btnignore
+            // 
+            this.btnignore.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btnignore.Image = ((System.Drawing.Image)(resources.GetObject("btnignore.Image")));
+            this.btnignore.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnignore.Location = new System.Drawing.Point(239, 7);
+            this.btnignore.Name = "btnignore";
+            this.btnignore.Size = new System.Drawing.Size(109, 44);
+            this.btnignore.TabIndex = 0;
+            this.btnignore.Text = "تایید";
+            this.btnignore.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnignore.UseVisualStyleBackColor = true;
+            this.btnignore.Click += new System.EventHandler(this.btnignore_Click);
+            // 
+            // btnno
+            // 
+            this.btnno.Image = ((System.Drawing.Image)(resources.GetObject("btnno.Image")));
+            this.btnno.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnno.Location = new System.Drawing.Point(124, 7);
+            this.btnno.Name = "btnno";
+            this.btnno.Size = new System.Drawing.Size(109, 44);
+            this.btnno.TabIndex = 4;
+            this.btnno.Text = "خیر";
+            this.btnno.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnno.UseVisualStyleBackColor = true;
+            this.btnno.Click += new System.EventHandler(this.btnno_Click);
+            // 
+            // btnok
+            // 
+            this.btnok.Image = global::persian_message.Properties.Resources.okj;
+            this.btnok.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnok.Location = new System.Drawing.Point(9, 7);
+            this.btnok.Name = "btnok";
+            this.btnok.Size = new System.Drawing.Size(109, 44);
+            this.btnok.TabIndex = 5;
+            this.btnok.Text = "قبول";
+            this.btnok.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnok.UseVisualStyleBackColor = true;
+            this.btnok.Click += new System.EventHandler(this.btnok_Click);
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.btnignore);
+            this.GroupBox1.Controls.Add(this.btnok);
+            this.GroupBox1.Controls.Add(this.btnabort);
+            this.GroupBox1.Controls.Add(this.btnno);
+            this.GroupBox1.Controls.Add(this.btnretry);
+            this.GroupBox1.Controls.Add(this.btncancel);
+            this.GroupBox1.Controls.Add(this.btnyes);
+            this.GroupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.GroupBox1.Location = new System.Drawing.Point(0, 97);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(353, 54);
+            this.GroupBox1.TabIndex = 7;
+            // 
+            // message
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(220)))), ((int)(((byte)(223)))));
+            this.ClientSize = new System.Drawing.Size(353, 151);
+            this.ControlBox = false;
+            this.Controls.Add(this.GroupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "message";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.message_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.GroupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnretry;
+        private System.Windows.Forms.Button btnabort;
+        private System.Windows.Forms.Button btncancel;
+        private System.Windows.Forms.Button btnyes;
+        private System.Windows.Forms.Button btnignore;
+        private System.Windows.Forms.Button btnno;
+        private System.Windows.Forms.Button btnok;
+        private System.Windows.Forms.Panel GroupBox1;
+    }
+}
+
---
+++ persian_message/message.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace persian_message
+{
+    public partial class message : Form
+    {
+        public message()
+        {
+            InitializeComponent();
+        }
+        public enum persianDialogResult
+        {
+            Yes,No,Ok,Cancel,Abort,Retry,Ignore
+        }
+        public enum persianMessageBoxButton
+        {
+            AbortRetryIgnore,Ok,OkCancel,RetryCancel,YesNo,YesNoCancel
+        }
+        public enum persianMessageBoxTheme
+        {
+            ghostWhite, silver, Gray
+        }
+        public enum persianMessageBoxIcon
+        {
+            Warning,Information, Error,Question
+
+        }
+        DialogResult _persianDialogResult;
+        private DialogResult persianResult
+        {
+            get { return _persianDialogResult; }
+            set { _persianDialogResult = value; }
+        }
+        private Image GetIcon(persianMessageBoxIcon messageIcon)
+        {
+            switch (messageIcon)
+            {
+                case persianMessageBoxIcon.Information:
+                    return Properties.Resources.info;
+                case persianMessageBoxIcon.Error:
+                    return Properties.Resources.eror;
+                case persianMessageBoxIcon.Question:
+
+                    return Properties.Resources.question;
+                case persianMessageBoxIcon.Warning:
+
+                    return Properties.Resources.warn;
+            }
+            
+                    return Properties.Resources.info;
+        }
+        private void SetButton(persianMessageBoxButton MessageButton)
+        {
+            GroupBox1.Top = label1.Height + 20;
+            GroupBox1.Left = label1.Left;
+           
+            //GroupBox1.Width = label1.Width;
+            
+            this.Left = GroupBox1.Width;
+
+            switch (MessageButton)
+            {
+                case persianMessageBoxButton.AbortRetryIgnore:
+                    btncancel.Visible = false;
+                    btnno.Visible = false;
+                    btnok.Visible = false;
+                    btnyes.Visible = false;
+                    btnignore.Visible = true;
+                    btnretry.Visible = true;
+                    btnabort.Visible = true;
+                    
+
+                    btnabort.Left = GroupBox1.Width - 336;
+                    btnretry.Left = GroupBox1.Width - 226;
+                    btnignore.Left = GroupBox1.Width - 116;
+                    break;
+                case persianMessageBoxButton.Ok:
+                    btncancel.Visible = false;
+                    btnno.Visible = false;
+                    btnok.Visible = true;
+                    btnyes.Visible = false;
+                    btnretry.Visible = false;
+                    btnabort.Visible = false;
+                    btnignore.Visible = false;
+
+                    
+                   btnok.Left = GroupBox1.Width -226;
+                    
+                    break;
+                case persianMessageBoxButton.OkCancel:
+                    btncancel.Visible = true;
+                    btnno.Visible = false;
+                    btnok.Visible = true;
+                    btnyes.Visible = false;
+                    btnretry.Visible = false;
+                    btnabort.Visible = false;
+                    btnignore.Visible = false;
+
+
+                    btnok.Left = GroupBox1.Width -158;
+                    btncancel.Left = GroupBox1.Width - 280;
+                    break;
+                case persianMessageBoxButton.RetryCancel:
+                    btncancel.Visible = true;
+                    btnno.Visible = false;
+                    btnok.Visible = false;
+                    btnyes.Visible = false;
+                    btnretry.Visible = true;
+                    btnabort.Visible = false;
+                    btnignore.Visible = false;
+
+
+                    btnretry.Left = GroupBox1.Width - 116;
+                    btncancel.Left = GroupBox1.Width - 336;
+                    break;
+                case persianMessageBoxButton.YesNo:
+                    btncancel.Visible = false;
+                    btnno.Visible = true;
+                    btnok.Visible = false;
+                    btnyes.Visible = true;
+                    btnretry.Visible = false;
+                    btnabort.Visible = false;
+                    btnignore.Visible = false;
+                   btnyes.Left = GroupBox1.Width - 158;
+                   btnno.Left = GroupBox1.Width - 280;
+                    break;
+                case persianMessageBoxButton.YesNoCancel:
+                    btncancel.Visible = true;
+                    btnno.Visible = true;
+                    btnok.Visible = false;
+                    btnyes.Visible = true;
+                    btnretry.Visible = false;
+                    btnabort.Visible = false;
+                    btnignore.Visible = false;
+
+
+                    btnyes.Left = GroupBox1.Width - 116;
+                    btnno.Left = GroupBox1.Width - 226;
+                    btncancel.Left = GroupBox1.Width - 336;
+                    break;
+
+
+
+            }
+        }
+
+        private void SetTheme(persianMessageBoxTheme MessageTheme)
+        {
+            switch (MessageTheme)
+            {
+                case persianMessageBoxTheme.silver:
+                    this.BackColor = Color.FromArgb(211, 220, 223);
+                    
+                    break;
+                case persianMessageBoxTheme.Gray:
+                    this.BackColor = Color.Gainsboro;
+                    break;
+                case persianMessageBoxTheme.ghostWhite:
+                    this.BackColor = Color.GhostWhite;
+                    break;
+            }
+        }
+
+
+        private void getsound(persianMessageBoxIcon Icon)
+        {
+            System.Media.SoundPlayer player= new System.Media.SoundPlayer();
+            switch (Icon)
+            {
+                case persianMessageBoxIcon.Error:
+                    player.Stream = Properties.Resources.sound_warning;
+                    player.Play();
+                    break;
+
+                case persianMessageBoxIcon.Warning:
+                    player.Stream = Properties.Resources.Message;
+                    player.Play();
+                    break;
+
+                case persianMessageBoxIcon.Question:
+                    player.Stream = Properties.Resources.sound_question;
+                    player.Play();
+                    break;
+                case persianMessageBoxIcon.Information:
+                    player.Stream = Properties.Resources.sound_information;
+                    player.Play();
+                    break;
+            }
+        }
+
+        public static DialogResult show(string Message)
+        {
+            message bax = new message();
+            bax.Text = " ";
+            bax.label1.Text = Message;
+            bax.SetButton(persianMessageBoxButton.Ok);
+            bax.SetTheme(persianMessageBoxTheme.Gray);
+            bax.ShowDialog();
+            return bax._persianDialogResult;
+        }
+        public static DialogResult show(string Message,string title)
+        {
+            message bax = new message();
+            bax.Text = title;
+            bax.label1.Text = Message;
+            bax.SetButton(persianMessageBoxButton.Ok);
+            bax.SetTheme(persianMessageBoxTheme.Gray);
+            bax.ShowDialog();
+            return bax._persianDialogResult;
+        }
+        public static DialogResult show(string Message, string title,persianMessageBoxButton messageButton)
+        {
+            message bax = new message();
+            bax.Text = title;
+            bax.label1.Text = Message;
+            bax.SetButton(messageButton);
+            bax.SetTheme(persianMessageBoxTheme.Gray);
+            bax.ShowDialog();
+            return bax._persianDialogResult;
+        }
+        public static DialogResult show(string Message, string title, persianMessageBoxButton messageButton,persianMessageBoxIcon messageIcon)
+        {
+            message bax = new message();
+            bax.Text = title;
+            bax.label1.Text = Message;
+            bax.SetButton(messageButton);
+            bax.pictureBox1.Image = bax.GetIcon(messageIcon);
+            bax.getsound(messageIcon);
+            bax.SetTheme(persianMessageBoxTheme.Gray);
+            bax.ShowDialog();
+            return bax._persianDialogResult;
+        }
+        public static DialogResult show(string Message, string title, persianMessageBoxButton messageButton, persianMessageBoxIcon messageIcon,persianMessageBoxTheme theme)
+        {
+            message bax = new message();
+            bax.Text = title;
+            bax.label1.Text = Message;
+            bax.SetButton(messageButton);
+            bax.pictureBox1.Image = bax.GetIcon(messageIcon);
+            bax.getsound(messageIcon);
+            bax.SetTheme(theme);
+            bax.ShowDialog();
+            return bax._persianDialogResult;
+        }
+        private void message_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnyes_Click(object sender, EventArgs e)
+        {
+            persianResult = DialogResult.Yes;
+            this.Close();
+        }
+
+        private void btnretry_Click(object sender, EventArgs e)
+        {
+            persianResult = DialogResult.Retry;
+            this.Close();
+        }
+
+        private void btnok_Click(object sender, EventArgs e)
+        {
+            persianResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void btnno_Click(object sender, EventArgs e)
+        {
+            persianResult = DialogResult.No;
+            this.Close();
+        }
+
+        private void btnabort_Click(object sender, EventArgs e)
+        {
+            persianResult = DialogResult.Abort;
+            this.Close();
+        }
+
+        private void btncancel_Click(object sender, EventArgs e)
+        {
+            persianResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        private void btnignore_Click(object sender, EventArgs e)
+        {
+            persianResult = DialogResult.Ignore;
+            this.Close();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
